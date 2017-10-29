fc12831b5ed370fdb53f35ed22091134178f5a6d
Dilly616 <unrelentinginsurrection@gmail.com>
2017-10-29 17:06:17 -0400

    Add files via upload

---
+++ sh_medkit.lua
@@ -0,0 +1,15 @@
+
+ITEM.name = "Medkit"
+ITEM.desc = "A first-aid kit"
+ITEM.model = "models/Items/HealthKit.mdl"
+ITEM.class = "weapon_medkit"
+ITEM.weaponCategory = "sidearm"
+ITEM.width = 1
+ITEM.height = 1
+ITEM.price = 120
+ITEM.flag = "y"
+ITEM.iconCam = {
+	ang	= Angle(0.33879372477531, 270.15808105469, 0),
+	fov	= 5.0470897275697,
+	pos	= Vector(0, 200, -1)
+}
